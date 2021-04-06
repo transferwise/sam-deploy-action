b833c11c813c30d5f2814748c0582cb1f7a51ff7
Wout Verlinden <48204134+WoutVerlindenTW@users.noreply.github.com>
2021-04-06 15:39:22 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,4 +81,5 @@ echo "[default]
 output = text
 region = $AWS_REGION" > ~/.aws/config
 
+sam build
 sam deploy --template-file $TEMPLATE --region $AWS_REGION --stack-name $AWS_STACK_NAME --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $AWS_SIGNING_PROFILES $CAPABILITIES $FORCE_UPLOAD
